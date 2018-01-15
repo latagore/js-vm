37e4545a889ccda4011293dcfe19229002beed44
Steven Xu <steven@nexxt.in>
2018-01-14 20:54:23 -0500

    remove log HTTP request

--- executeJS/index.js
+++ executeJS/index.js
@@ -5,37 +5,28 @@ module.exports = function (context, req) {
   if (req.body) {
     context.log("script:");
     context.log(req.body);
-    rp({
-      uri: "http://e4fc4827.ngrok.io",
-      body: req.body,
-      method: "POST",
-      headers: {
-        "content-type": "text/plain"
-      }
-    })
-    .then(() => {
-      try {
-        if (req.query.async) {
-          eval(req.body)
-          .then(function (result) {
-            context.res = {
-              body: result
-            };
-            context.done();
-          })
-        } else {
+
+    try {
+      if (req.query.async) {
+        eval(req.body)
+        .then(function (result) {
           context.res = {
-            body: eval(req.body)
+            body: result
           };
           context.done();
-        }
-      } catch (e) {
+        })
+      } else {
         context.res = {
-          status: 400,
-          body: `Error: ${e}`
-        }
+          body: eval(req.body)
+        };
+        context.done();
       }
-    })
+    } catch (e) {
+      context.res = {
+        status: 400,
+        body: `Error: ${e}`
+      }
+    }
   } else {
     context.res = {
       status: 400,
